7009a78e93b3748d1c30d68bc1fb0491cf10cbce
zisong,zhang <zhangzis@oregonstate.edu>
2020-06-03 23:10:58 -0700

    write some assignment function

--- api/assignments.js
+++ api/assignments.js
@@ -1,13 +1,35 @@
 const router = require('express').Router();
-
+const {
+    AssignmentSchema,
+    getAssignmentById,
+    insertNewAssignment,
+    changeAssignment,
+    deleteAssignmentById,
+    getAssignmentSubmissionByAssignmentId
+    } = require('../models/assignments');
 
 router.post('/', (req, res, next) =>{
+
     res.send({"name":"post:assignments/"})
 })
 
 router.get('/:id', (req, res, next) =>{
+    id = parseInt(req.params.businessid);
+    try {
+        const assignment_info = await getAssignmentById(id);
+        if (assignment_info){
+            res.status(200).send(assignment_info);
+    }else{
+        next();
+    }
+    }catch(err){
+        console.error(err);
+        res.status(500).send({
+            error: "Unable to find assignment."
+        });
+    }
     res.send({"name":"get:assignments/:id"})
-})
+});
 
 router.patch('/:id', (req, res, next) =>{
     res.send({"name":"patch:assignments/:id"})
--- models/assignments.js
+++ models/assignments.js
@@ -1,1 +1,66 @@
 const mysqlPool = require('../lib/mysqlPool');
+
+const AssignmentSchema = {
+    courseId: { required: true },
+    title: { required: true },
+    points: { required: true },
+    due: { required: true }
+  };
+exports.AssignmentSchema = AssignmentSchema;
+
+
+// thisn function to get assignment by ID
+async function getAssignmentById(id){
+    const [ results ] = await mysqlPool.query(
+        'SELECT * FROM assignment WHERE id = ?',
+        [ id ]
+    );
+    return results[0];
+}
+exports.getAssignmentById = getAssignmentById;
+
+
+// this function to INSERT a new assignment
+async function insertNewAssignment(new_assignment){
+    new_assignment = extractValidFields(new_assignment, AssignmentSchema);
+    const [ result ] = await mysqlPool.query(
+        'INSERT INTO assignment SET ?',
+        new_assignment
+    );
+    return result.insert_id;
+}
+exports.insertNewAssignment = insertNewAssignment;
+
+
+// this function is to update assignment
+async function changeAssignment(changed_assignment, id){
+    changed_assignment = extractValidFields(changed_assignment, AssignmentSchema);
+    const [ result ] = await mysqlPool.query(
+        'UPDATE assignment SET ? WHERE id = ?',
+        [changed_assignment, id]
+    );
+    return result.rows > 0;
+}
+exports.changeAssignment = changeAssignment;
+
+
+// this function is to DELETE assignment
+async function deleteAssignmentById(id){
+    const [ results ] = await mysqlPool.query(
+        'DELETE FROM assignment WHERE id = ?',
+        [ id ]
+    );
+    return results.rows > 0;
+}
+exports.deleteAssignmentById = deleteAssignmentById
+
+
+// this function is to get submission by assignment_id
+async function getAssignmentSubmissionByAssignmentId(assignment_id){
+    const [ results ] = await mysqlPool.query(
+        'SELECT * FROM submission WHERE assignmentid = ?',
+        [ assignment_id ]
+    );
+    return results;
+}
+exports.getAssignmentSubmissionByAssignmentId = getAssignmentSubmissionByAssignmentId;
